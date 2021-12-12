3cfbfe85075113e8be7d696ce21a107c8b30b7e6
strarsis <strarsis@gmail.com>
2021-12-12 16:59:20 +0100

    Initial commit (add Sage 10 base files).

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: ['./app/**/*.php', './resources/**/*.{php,vue,js}'],
+  },
+  jit: true,
+  darkMode: false,
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+};
