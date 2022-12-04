aab5b1b6f21eb3e298ad5564cdc04fb1b10e9eee
strarsis <strarsis@gmail.com>
2022-12-04 02:24:12 +0100

    Fix name (pattern, not part).

--- patterns/some-part.php
+++ patterns/some-part.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Title: Some part
- * Slug: sage/some-part
+ * Title: Some pattern
+ * Slug: sage/some-pattern
  * Categories: header
- * Description: The description of an useful block part.
- * Keywords: some, part, block, custom
+ * Description: The description of an useful block pattern.
+ * Keywords: some, pattern, block, custom
  * Block Types: core/group, core/heading, core/paragraph
  *
  * @see https://wordpress.stackexchange.com/a/398395/134384
@@ -13,8 +13,8 @@
 ?>
 <!-- wp:group {"templateLock":"all","align":"full","className":"custom-class-if-needed"} -->
 <div class="wp-block-group alignfull some-custom-class-if-needed">
-  <!-- wp:heading --><h3>Some heading that belongs to this block part.</h3><!-- /wp:heading -->
-  <!-- wp:paragraph --><p>Some paragraph that belongs to this block part.</p><!-- /wp:paragraph -->
-  <!-- wp:paragraph --><p>Some other paragraph that belongs to this block part.</p><!-- /wp:paragraph -->
+  <!-- wp:heading --><h3>Some heading that belongs to this block pattern.</h3><!-- /wp:heading -->
+  <!-- wp:paragraph --><p>Some paragraph that belongs to this block pattern.</p><!-- /wp:paragraph -->
+  <!-- wp:paragraph --><p>Some other paragraph that belongs to this block pattern.</p><!-- /wp:paragraph -->
 </div>
 <!-- /wp:group -->
